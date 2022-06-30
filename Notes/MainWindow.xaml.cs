34c8349877636406f50d8706a2d9f341a30c4548
Poli <pke-erbe@t-online.de>
2022-06-30 14:53:36 +0200

    Json Serilialisation and btn fixes

--- Notes/MainWindow.xaml.cs
+++ Notes/MainWindow.xaml.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 using System.ComponentModel;
 using System.Collections.ObjectModel;
 using System.IO;
+using System.Text.Json;
+
 using Microsoft.Win32;
 using System.Windows;
 using System.Windows.Controls;
@@ -25,24 +27,24 @@ namespace Notes
     public partial class MainWindow : Window
     {
         ObservableCollection<Notice> notes = new ObservableCollection<Notice>();
-        string path = @"C:\Users\ka-er\Desktop\TestOrdner\Notizen.txt";
-        FileInfo file;
+        private const string jsonFileName = "Notes.json";
 
         public MainWindow()
         {
             InitializeComponent();
 
-            Notices.ItemsSource = notes;
-            file = new FileInfo(path);
-
-            if (!string.IsNullOrEmpty(path))
+            if (!File.Exists(jsonFileName))
             {
-                string[] FileContent = File.ReadAllLines(path);
-                foreach (string FileContentItem in FileContent)
-                {
-                    notes.Add(new Notice(FileContentItem));
-                } 
+                File.Create("Notes.json").Close();
             }
+            string jsonFile = File.ReadAllText(jsonFileName);
+            if (!string.IsNullOrEmpty(jsonFile))
+            {
+                notes = JsonSerializer.Deserialize<ObservableCollection<Notice>>(jsonFile);
+            }
+
+            Notices.ItemsSource = notes;
+            
             if (notes.Count > 0)
             {
                 Notices.Visibility = Visibility.Visible;
@@ -51,14 +53,11 @@ namespace Notes
 
         private void addButton_Click(object sender, RoutedEventArgs e)
         {
-            if (newNotice.Text == "")
+            if (!string.IsNullOrEmpty(newNoticeTitel.Text) && !string.IsNullOrEmpty(newNoticeText.Text))
             {
-                return;
-            }
-            else
-            {
-                notes.Add(new Notice(newNotice.Text));
-                newNotice.Text = "";
+                notes.Add(new Notice(newNoticeTitel.Text, newNoticeText.Text));
+                newNoticeTitel.Text = "";
+                newNoticeText.Text = "";
             }
             if (notes.Count > 0)
             {
@@ -75,31 +74,47 @@ namespace Notes
             }
         }
 
-        private void changeButton_Click(object sender, RoutedEventArgs e)
+        private void overwriteButton_Click(object sender, RoutedEventArgs e)
         {
-            (Notices.SelectedItem as Notice).Text = newNotice.Text;
-            newNotice.Text = "";
+            if (!string.IsNullOrEmpty(newNoticeTitel.Text) && !string.IsNullOrEmpty(newNoticeText.Text))
+            {
+                (Notices.SelectedItem as Notice).Text = newNoticeTitel.Text;
+                (Notices.SelectedItem as Notice).Note = newNoticeText.Text;
+                newNoticeTitel.Text = "";
+                newNoticeText.Text = "";
+            }
+        }
+
+        private void openButton_Click(object sender, RoutedEventArgs e)
+        {
+            newNoticeTitel.Text = (Notices.SelectedItem as Notice).Text;
+            newNoticeText.Text = (Notices.SelectedItem as Notice).Note;
         }
 
         private void Save_Click(object sender, RoutedEventArgs e)
         {
-            SaveFileDialog saveFileDialog = new SaveFileDialog();
-            saveFileDialog.InitialDirectory = @"C:\Users\ka-er\Desktop\TestOrdner";
-            saveFileDialog.Filter = "Text file (*.txt)|*.txt|C# file (*.cs)|*.cs";
-            if (saveFileDialog.ShowDialog() == true)
+            string jsonFile = JsonSerializer.Serialize(notes);
+            File.WriteAllText(jsonFileName, jsonFile);
+        }
+
+        private void Notices_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if(Notices.SelectedItem != null)
             {
-                File.WriteAllText(path, "");
-                using (StreamWriter sw = file.AppendText())
-                {
-                    foreach (Notice notice in notes)
-                    {
-                        sw.WriteLine(notice.Text);
-                    }
-                }
+                deleteButton.IsEnabled = true;
+                overwriteButton.IsEnabled = true;
+                openButton.IsEnabled = true;
+            }
+            else
+            {
+                deleteButton.IsEnabled = false;
+                overwriteButton.IsEnabled = false;
+                openButton.IsEnabled = false;
             }
         }
     }
 
+    [Serializable]
     class Notice : INotifyPropertyChanged
     {
         private string _text;
@@ -113,17 +128,19 @@ namespace Notes
                 }
             }
         }
+        public string Note { set; get; }
 
-        public Notice(string _text)
+        public Notice(string text, string note)
         {
-            Text = _text;
+            Text = text;
+            Note = note;
         }
 
         public event PropertyChangedEventHandler? PropertyChanged;
 
         public void NotifyPropertyChanged(string propName)
         {
-            if (this.PropertyChanged != null)
+            if (this.PropertyChanged != null && this._text != "")
                 this.PropertyChanged(this, new PropertyChangedEventArgs(propName));
         }
     }
